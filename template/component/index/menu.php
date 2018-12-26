d4eb1fdbeb82dcffdd42a77fdfb93a5f27161907
ThaoPham <phamthithao1711@gmail.com>
2018-12-26 13:09:29 +0700

    update

--- template/component/index/menu.php
+++ template/component/index/menu.php
@@ -1,14 +1,14 @@
 <!-- ----------------list-nav----------------------------------->
 		<div class="list-nav">
 			<nav class="navbar navbar-expand-lg navbar-light bg-light list-nav ">
-				<a class="nav-link active" href="quanlitintuc.html">Home</a>
-				<a class="nav-link" href="#">Xã hội</a>
-				<a class="nav-link" href="#">Giáo dục</a>
-				<a class="nav-link" href="#">Kinh tế</a>
-				<a class="nav-link" href="#">Pháp luật</a>
-				<a class="nav-link" href="#">Giải trí</a>
-				<a class="nav-link" href="#">Thể thao</a>
-				<a class="nav-link" href="#">Công nghệ</a>
+				<a class="nav-link active link" href="quanlitintuc.html">Home</a>
+				<a class="nav-link link" href="#">Xã hội</a>
+				<a class="nav-link link" href="#">Giáo dục</a>
+				<a class="nav-link link" href="#">Kinh tế</a>
+				<a class="nav-link link" href="#">Pháp luật</a>
+				<a class="nav-link link" href="#">Giải trí</a>
+				<a class="nav-link link" href="#">Thể thao</a>
+				<a class="nav-link link" href="#">Công nghệ</a>
 			</nav>
 		</div>
 	</div>
--- template/posts/viewdetail.php
+++ template/posts/viewdetail.php
@@ -4,7 +4,41 @@ require_once "template/component/index/menu.php";
 require_once "template/component/index/hotnews.php";
 ?>
 <div class="col-md-8 postdetail">
-	Nội dung ở đây
+	<div class="background-baibao">
+		<div class="top-background">
+			<div class="list">
+				<nav class=" navbar navbar-expand-lg navbar-light bg-light list-noidung">
+					<a class="nav-link active thanhnoidung" href="quanlitintuc.html">Home</a>
+					<a class="nav-link thanhnoidung" href="#"><i class="fas fa-angle-right"></i></a>
+					<a class="nav-link thanhnoidung" href="#">Xã hội</a>
+				</nav>
+			</div>
+		</div>
+		<div class="content-noidung">
+			<div class="text-post noidung">
+			<h2>Vụ 152 du khách Việt Nam mất tích ở Đài Loan: Công ty du lịch làm visa nói gì?</h2>
+			<ul>
+								<li><i class="far fa-user"></i> Bimbim</li>
+								<li><i class="far fa-clock"></i>1 giờ</li>
+								<li><i class="far fa-calendar-alt"></i> 17/11/2018</li>
+			</ul>
+			</div>
+			<h5>Công ty làm visa cho đoàn du khách cho biết, họ chỉ chịu trách nhiệm làm visa, không thực hiện dẫn đoàn tại Đài Loan.</h5>
+			<p>Hôm qua (ngày 25/12), Cục du lịch Đài Loan xác nhận có 152 người trong tổng số 153 khách du lịch Việt Nam biến mất khi đặt chân đến sân bay tại thành phố Cao Hùng. Thông tin này được công bố khiến dư luận trong nước và quốc tế vô cùng hoang mang.</p>
+			<p>Theo tìm hiểu, Công ty du lịch Holidays (trụ sở tại quận Gò Vấp, TP.HCM) chính là đơn vị làm visa cho đoàn du khách trên.</p>
+			<p>Sáng ngày 26/12, liên quan đến sự việc, đại diện Công ty du lịch Holidays cho biết họ chỉ chịu trách nhiệm làm visa cho đoàn du khách, còn việc dẫn đoàn sang Đài Loan lại do đơn vị khác thực hiện. </p>
+			<div class="anh-baibao">
+				<img src="template/assets/images/ctydulic.png" alt="" class="anhbaibao">
+				<p class="chuthich">Trụ sở Công ty du lịch Holidays tại TP.HCM</p>
+			</div>
+			<p>Theo đại diện của công ty, đơn vị không liên can đến vụ việc nhóm người Việt mất tích ở Đài Loạn nên không cung cấp thông tin gì thêm. </p>
+			<p>Hiện lãnh đạo Công ty Holidays đã được Sở Du Lịch TP. HCM triệu tập để làm việc do liên quan đến việc làm visa cho đoàn khách.</p>
+			<p>Trước đó, ngày 25/12, Cục du lịch Đài Loan thông tin đã có 152 người trong nhóm khách du lịch bao gồm 153 người đến từ Việt Nam đã biến mất khi đến sân bay tại thành phố Cao Hùng.Đoàn du lịch bao gồm 4 nhóm du khách đã bay đến Cao Hùng từ ngày 21-23/12. Tuy nhiên, đến 25/12 công ty quản lý du lịch Đông Sâm - nơi đã tổ chức tour cho nhóm khách du lịch này đã thông báo với Cục Du lịch rằng họ không thể tìm thấy được 152 người trên tổng số 153 khách du lịch.</p>
+			<p>Ngay trong ngày 25/12, Cơ quan chức năng Đài Loan cho biết, họ đã thành lập một đội chuyên trách để tìm kiếm 152 du khách Việt Nam mất tích.Đến nay, Cơ quan di trú (NIA) tại Cao Hùng đã có được hành trình của nhóm khách du lịch nói trên, và hiện vẫn đang phối hợp tích cực với lực lượng cảnh sát để tìm kiếm nhóm người mất tích.</p>
+			<h5>>>Đọc thêm: <a href=""> Hơn 150 du khách người Việt Nam nghi bỏ trốn tại Đài Loan</a></h5>
+		</div>
+		
+	</div>
 </div>
 <?php
 // Hiển thị SiderBar 
