7daeb6a6933efcc80360b660649dc7c1e1cd3c31
ThaoPham <phamthithao1711@gmail.com>
2018-12-27 02:51:57 +0700

    update

--- template/categories/viewcate.php
+++ template/categories/viewcate.php
@@ -4,7 +4,137 @@ require_once "template/component/index/menu.php";
 require_once "template/component/index/hotnews.php";
 ?>
 <div class="col-md-8 postdetail">
-	
+	<div class="background-baibao">
+		<div class="top-background hienthi">
+			<nav class=" navbar navbar-expand-lg navbar-light thanhtieude">
+				<a class="nav-link thanhluachon" href="#">Xã hội</a>
+
+			</nav>
+			<div class="left">
+				
+				<div class="list-baibao">
+					<div class="row">
+						<div class="col-lg-5">
+							<div class="div-picture">
+								<img src="template/assets/images/cuchoami.jpg" alt="" class="picture-list" style="width:270px; height: 190px; object-fit: cover;object-position: center center;">
+							</div>
+						</div>
+						<div class="col-lg-7">
+							<div class="text-post">
+								<a href=""><h5>Cuối cùng mọi thứ sẽ ổn nếu nó chưa ổn thì chưa phải cuối cùng</h5></a>
+								<ul>
+									<li><i class="far fa-user"></i> Bimbim</li>
+									<li><i class="far fa-clock"></i>1 giờ</li>
+									<li><i class="far fa-calendar-alt"></i> 17/11/2018</li>
+								</ul>
+								<p>Cúc họa mi loài hoa gọi mùa đông về đang ngập tràn sắc trắng tinh khôi trên mọi góc phố của Hà Nội.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="list-baibao">
+					<div class="row">
+						<div class="col-lg-5">
+							<div class="div-picture">
+								<img src="template/assets/images/cuchoami.jpg" alt="" class="picture-list" style="width:270px; height: 190px; object-fit: cover;object-position: center center;">
+							</div>
+						</div>
+						<div class="col-lg-7">
+							<div class="text-post">
+								<a href=""><h5>Cuối cùng mọi thứ sẽ ổn nếu nó chưa ổn thì chưa phải cuối cùng</h5></a>
+								<ul>
+									<li><i class="far fa-user"></i> Bimbim</li>
+									<li><i class="far fa-clock"></i>1 giờ</li>
+									<li><i class="far fa-calendar-alt"></i> 17/11/2018</li>
+								</ul>
+								<p>Cúc họa mi – loài hoa gọi mùa đông về đang ngập tràn sắc trắng tinh khôi trên mọi góc phố của Hà Nội.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="list-baibao">
+					<div class="row">
+						<div class="col-lg-5">
+							<div class="div-picture">
+								<img src="template/assets/images/cuchoami.jpg" alt="" class="picture-list" style="width:270px; height: 190px; object-fit: cover;object-position: center center;">
+							</div>
+						</div>
+						<div class="col-lg-7">
+							<div class="text-post">
+								<a href=""><h5>Cuối cùng mọi thứ sẽ ổn nếu nó chưa ổn thì chưa phải cuối cùng</h5></a>
+								<ul>
+									<li><i class="far fa-user"></i> Bimbim</li>
+									<li><i class="far fa-clock"></i>1 giờ</li>
+									<li><i class="far fa-calendar-alt"></i> 17/11/2018</li>
+								</ul>
+								<p>Cúc họa mi loài hoa gọi mùa đông về đang ngập tràn sắc trắng tinh khôi trên mọi góc phố của Hà Nội.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="list-baibao">
+					<div class="row">
+						<div class="col-lg-5">
+							<div class="div-picture">
+								<img src="template/assets/images/cuchoami.jpg" alt="" class="picture-list" style="width:270px; height: 190px; object-fit: cover;object-position: center center;">
+							</div>
+						</div>
+						<div class="col-lg-7">
+							<div class="text-post">
+								<a href=""><h5>Cuối cùng mọi thứ sẽ ổn nếu nó chưa ổn thì chưa phải cuối cùng</h5></a>
+								<ul>
+									<li><i class="far fa-user"></i> Bimbim</li>
+									<li><i class="far fa-clock"></i>1 giờ</li>
+									<li><i class="far fa-calendar-alt"></i> 17/11/2018</li>
+								</ul>
+								<p>Cúc họa mi – loài hoa gọi mùa đông về đang ngập tràn sắc trắng tinh khôi trên mọi góc phố của Hà Nội.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="list-baibao">
+					<div class="row">
+						<div class="col-lg-5">
+							<div class="div-picture">
+								<img src="template/assets/images/cuchoami.jpg" alt="" class="picture-list" style="width:270px; height: 190px; object-fit: cover;object-position: center center;">
+							</div>
+						</div>
+						<div class="col-lg-7">
+							<div class="text-post">
+								<a href=""><h5>Cuối cùng mọi thứ sẽ ổn nếu nó chưa ổn thì chưa phải cuối cùng</h5></a>
+								<ul>
+									<li><i class="far fa-user"></i> Bimbim</li>
+									<li><i class="far fa-clock"></i>1 giờ</li>
+									<li><i class="far fa-calendar-alt"></i> 17/11/2018</li>
+								</ul>
+								<p>Cúc họa mi – loài hoa gọi mùa đông về đang ngập tràn sắc trắng tinh khôi trên mọi góc phố của Hà Nội.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="list-baibao">
+					<div class="row">
+						<div class="col-lg-5">
+							<div class="div-picture">
+								<img src="template/assets/images/cuchoami.jpg" alt="" class="picture-list" style="width:270px; height: 190px; object-fit: cover;object-position: center center;">
+							</div>
+						</div>
+						<div class="col-lg-7">
+							<div class="text-post">
+								<a href=""><h5>Cuối cùng mọi thứ sẽ ổn nếu nó chưa ổn thì chưa phải cuối cùng</h5></a>
+								<ul>
+									<li><i class="far fa-user"></i> Bimbim</li>
+									<li><i class="far fa-clock"></i>1 giờ</li>
+									<li><i class="far fa-calendar-alt"></i> 17/11/2018</li>
+								</ul>
+								<p>Cúc họa mi – loài hoa gọi mùa đông về đang ngập tràn sắc trắng tinh khôi trên mọi góc phố của Hà Nội.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>	
 </div>
 <?php
 // Hiển thị SiderBar 
--- template/component/index/menu.php
+++ template/component/index/menu.php
@@ -2,8 +2,8 @@
 		<div class="list-nav">
 			<nav class="navbar navbar-expand-lg navbar-light bg-light list-nav ">
 				<a class="nav-link active link" href="index.php"><i class="fas fa-home"></i> Home</a>
-				<a class="nav-link link" href="#" onclick="openCity(event, 'xahoi')" id="defaultOpen">Xã hội</a>
-				<a class="nav-link link" href="#" onclick="openCity(event, 'giaoduc')">Giáo dục</a>
+				<a class="nav-link link" href="viewcate.php" >Xã hội</a>
+				<a class="nav-link link" href="#" >Giáo dục</a>
 				<a class="nav-link link" href="#">Kinh tế</a>
 				<a class="nav-link link" href="#">Pháp luật</a>
 				<a class="nav-link link" href="#">Giải trí</a>
