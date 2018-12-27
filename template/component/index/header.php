6fce75a9adc431a50a6d0de272471508bcaa80db
ThaoPham <phamthithao1711@gmail.com>
2018-12-27 21:38:17 +0700

    up

--- template/component/index/header.php
+++ template/component/index/header.php
@@ -14,7 +14,7 @@
 			<nav class="navbar navbar-expand-lg navbar-light bg-light top-nav ">
 				<span class="navbar-brand mb-0 h1"><a href="index.php"><img src="template/assets/images/free_horizontal_on_white_by_logaster (1).png" alt="" class="logo"></a></span>
 				<div class="input-group mb-0 search">
-					<form action="index.php?act=search" method="POST">
+					<form action="index.php?act=search" method="POST" class="group-form form-search">
 						<input type="text" class="form-control" placeholder="Nhập nội dung tìm kiếm" name="inputword" aria-label="search" aria-describedby="basic-addon2">
 						<div class="input-group-append">
 							<button class="btn btn-outline-secondary button-search" type="submit" name="btn-submit"><i class="fas fa-search"></i></button>
