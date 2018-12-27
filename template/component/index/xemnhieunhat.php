19559d05e9c8675edde798f305a801e42a488094
ThaoPham <phamthithao1711@gmail.com>
2018-12-27 10:40:12 +0700

    up

--- template/component/index/xemnhieunhat.php
+++ template/component/index/xemnhieunhat.php
@@ -1,11 +1,1 @@
 <!-- Tin xem nhiều nhất -->
-<div class="tintucnoibat">
-					<div class="content-tintucnoibat">
-						<div class="title">
-							Tin xem nhiều
-						</div>
-						<div id="main">
-
-						</div>
-					</div>		
-				</div>
